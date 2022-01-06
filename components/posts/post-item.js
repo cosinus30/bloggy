2701efeb3bc6c21bfe900fe09a7fff68db61f095
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-01-06 23:00:48 +0300

    chore: indentation & rounded corners

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,29 +1,35 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function PostItem({post}) {
-    const { title, image, summary, date, slug } = post;
+export default function PostItem({ post }) {
+  const { title, image, summary, date, slug } = post;
 
-    const formattedDate = new Date(date).toLocaleDateString("en-US", {
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    })
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-    return (
-        <Link href={`/posts/${slug}`}>
-            <a className="hover:shadow-lg hover:shadow-black">
-                <div>
-                    <Image src={image.src} alt={image.alt} width={600} height={400} className="rounded-lg object-cover h-full w-full"/>
-                </div>
-                <div className="mx-3 my-4 flex flex-col">
-                    <div className="flex flex-row justify-between align-middle">
-                        <h3 className="text-2xl font-semibold">{title}</h3>
-                        <time className="self-end text-xs text-zinc-500 italic my-auto">{formattedDate}</time>
-                    </div>
-                    <p className="mt-2 text-lg">{summary}</p>
-                </div>
-            </a>        
-        </Link>
-    ) 
+  return (
+    <Link href={`/posts/${slug}`}>
+      <a className="hover:shadow-lg hover:shadow-black">
+        <div>
+          <Image
+            src={image.src}
+            alt={image.alt}
+            width={600}
+            height={400}
+            className="rounded-t-lg object-cover h-full w-full"
+          />
+        </div>
+        <div className="mx-3 my-4 flex flex-col rounded-b-lg">
+          <div className="flex flex-row justify-between align-middle">
+            <h3 className="text-2xl font-semibold">{title}</h3>
+            <time className="self-end text-xs text-zinc-500 italic my-auto">{formattedDate}</time>
+          </div>
+          <p className="mt-2 text-lg">{summary}</p>
+        </div>
+      </a>
+    </Link>
+  );
 }
