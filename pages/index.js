9ef060c402fd06d61d1cbd9402c5c2221f44b9da
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-01-08 19:33:43 +0300

    feat: add real data source for all& featured posts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import FeaturedPosts from "../components/home/featured-posts";
 import Hero from "../components/home/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
 export default function Home({ posts }) {
   console.log('hey: ', posts)
@@ -18,52 +19,13 @@ export default function Home({ posts }) {
 }
 
 export async function getStaticProps() {
-  const DUMMY_POSTS = [
-    {
-      title: "Post 1",
-      slug: "post-1",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-    {
-      title: "Post 2",
-      slug: "post-2",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-    {
-      title: "Post 3",
-      slug: "post-3",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-    {
-      title: "Post 4",
-      slug: "post-4",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-  ];
+  const posts = await getFeaturedPosts();
+  console.log(posts)
 
   return {
     props: {
-      posts: DUMMY_POSTS,
+      posts: posts,
     },
+    revalidate: 60 * 60 * 12,
   };
 }
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import PostsGrid from '../../components/posts/posts-grid'
+import Head from "next/head";
+import PostsGrid from "../../components/posts/posts-grid";
+import { getAllPosts } from "../../lib/posts-util";
 
-export default function Posts({posts}) {
+export default function Posts({ posts }) {
   return (
     <div className="">
       <Head>
@@ -9,63 +10,19 @@ export default function Posts({posts}) {
         <meta name="description" content="You can see all my articles here." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='text-center text-6xl my-8'>
-        All Posts
-      </h1>
-      <PostsGrid posts={posts}/>
+      <h1 className="text-center text-6xl my-8">All Posts</h1>
+      <PostsGrid posts={posts} />
     </div>
-  )
+  );
 }
 
-export function getStaticProps() {
-  const DUMMY_DATA = [
-    {
-      title: "Post 1",
-      slug: "post-1",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-    {
-      title: "Post 2",
-      slug: "post-2",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-    {
-      title: "Post 3",
-      slug: "post-3",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-    {
-      title: "Post 4",
-      slug: "post-4",
-      date: "2020-01-01",
-      summary: "This is a post",
-      image: {
-        src: "/me.jpeg",
-        alt: "Image showing Yunus Kerem Turk",
-      },
-    },
-  
-    
-  ]
-  
+export async function getStaticProps() {
+  const allPosts = await getAllPosts();
+
   return {
     props: {
-      posts: DUMMY_DATA
-    }
-  }
+      posts: allPosts,
+    },
+    revalidate: 60 * 60 * 12,
+  };
 }
