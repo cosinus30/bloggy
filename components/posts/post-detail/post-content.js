c16a2661302254ad5ec419382028e6a9fbf03ea8
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-01-09 23:25:58 +0300

    chore: add custom renderer to utilize next image

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,11 +1,38 @@
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 export default function PostContent({ image, title, date, content }) {
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div>
+            <Image
+              src={`${image.properties.src}`}
+              alt={image.alt}
+              width={800}
+              height={500}
+              layout="responsive"
+              className="object-cover rounded-lg"
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    
+  };
+
   return (
     <article className="flex flex-col gap-y-12">
       <PostHeader image={image} title={title} />
-      <ReactMarkdown className="prose prose-invert">{content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers} className="prose prose-invert mx-auto">{content}</ReactMarkdown>
     </article>
   );
 }
