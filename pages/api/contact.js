c3c7048fed4d50659b6e26f1b5014ba262e90c2e
Yunus Kerem Turk <40324244+cosinus30@users.noreply.github.com>
2022-01-10 16:57:29 +0300

    feat: connect mongodb to apis

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,19 +1,33 @@
-export default function handler(req, res) {
+import { password, username } from "../../constants/credentials";
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (!email || !validateEmail(email) || !name || !name.trim() === "" || !message || !message.trim() === "") {
       res.status(400).json({ message: "Invalid input", error: true });
       return;
     }
-    //TODO store it in a database
-    const newMessage = {
-        email,
-        name,
-        message
-    }
-    console.log(newMessage)
-    res.status(200).json({ success: true, message: "Successfully stored the message!" });
 
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to database failed", error: true });
+      return;
+    }
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    try {
+      await insertDocument(client, newMessage);
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed", error: true });
+    }
+    res.status(200).json({ success: true, message: "Your message is successfully stored." });
   }
 }
 
@@ -22,3 +36,15 @@ const validateEmail = (email) => {
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
 };
+
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://${username}:${password}@cluster0.5rv5l.mongodb.net/messages?retryWrites=true&w=majority`
+  );
+  return client;
+}
+
+async function insertDocument(client, data) {
+  const db = client.db();
+  await db.collection("contacts").insertOne({ ...data });
+}
